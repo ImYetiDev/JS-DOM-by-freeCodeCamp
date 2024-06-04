5a53e8d84e291148be1910add432c18f4a66efe7
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2024-06-04 00:54:49 -0500

    feat: Add "Volver" button functionality to go back to the homepage

--- color-hex-aleatorio/app.js
+++ color-hex-aleatorio/app.js
@@ -1,5 +1,6 @@
 const boton = document.querySelector('button');
 const color = document.getElementById('color');
+const btnVolver = document.getElementById('volver');
 
 function generarColor() {
   let digitos = '0123456789ABCDEF';
@@ -14,4 +15,8 @@ boton.addEventListener('click', () => {
   let nuevoColor = generarColor();
   color.textContent = nuevoColor;
   document.body.style.backgroundColor = nuevoColor;
+});
+
+btnVolver.addEventListener('click', () => {
+  location.href = '../index.html';
 });
